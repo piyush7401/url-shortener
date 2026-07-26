7bd6f5cd58db8ad55fb6673147a5fb48cbba7ddc
vikasmittal081 <vikasmittal353@gmail.com>
2026-07-26 11:28:26 +0530

    InMemoryUrlRepository added

---
+++ src/main/java/com/urlshortener/url_shortener/repository/InMemoryUrlRepository.java
@@ -0,0 +1,43 @@
+package com.urlshortener.url_shortener.repository;
+
+import com.urlshortener.url_shortener.model.UrlMapping;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryUrlRepository implements UrlRepository {
+
+    private ConcurrentHashMap<String, UrlMapping> map = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, UrlMapping> longUrlDuplicate = new ConcurrentHashMap<>();
+    @Override
+    public void saveToDB(UrlMapping mapping){
+        String shortUrl = mapping.getShortUrl();
+        String longUrl = mapping.getLongUrl();
+        map.put(shortUrl, mapping);
+        longUrlDuplicate.put(longUrl, mapping);
+    }
+
+    @Override
+    public UrlMapping getLongURL(String shortUrl) {
+        return map.get(shortUrl);
+    }
+
+    @Override
+    public boolean checkShortUrlExist(String shortUrl) {
+        if(map.containsKey(shortUrl)){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean checkLongUrlExist(String longUrl) {
+        if(longUrlDuplicate.containsKey(longUrl)){
+            UrlMapping mapping = longUrlDuplicate.get(longUrl);
+            if(mapping.isExpired()){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/com/urlshortener/url_shortener/repository/UrlRepository.java
+++ src/main/java/com/urlshortener/url_shortener/repository/UrlRepository.java
@@ -5,6 +5,6 @@ import com.urlshortener.url_shortener.model.UrlMapping;
 public interface UrlRepository {
     void saveToDB(UrlMapping mapping);
     UrlMapping getLongURL(String shortUrl);
-    boolean checkShortURL(String shortUrl);
+    boolean checkShortUrlExist(String shortUrl);
     boolean checkLongUrlExist(String longUrl);
 }
